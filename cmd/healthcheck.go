c3544e31c7479ebb6e79966451448ba3f1a0cc71
Grant Arnold <grant.arnold@safe.com>
2022-10-20 09:23:40 -0700

    Simplify healthcheck output.

--- cmd/healthcheck.go
+++ cmd/healthcheck.go
@@ -6,10 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"reflect"
-	"strings"
 
-	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 )
 
@@ -23,12 +20,18 @@ type Healthcheck struct {
 var healthcheckCmd = &cobra.Command{
 	Use:   "healthcheck",
 	Short: "Retrieves the health status of FME Server",
-	Long:  `Retrieves the health status of FME Server. The health status is normal if the FME Server REST API is responsive. Note that this endpoint does not require authentication. Load balancer or other systems can monitor FME Server using this endpoint without supplying token or password credentials.`,
+	Long: `Retrieves the health status of FME Server. The health status is normal if the FME Server REST API is responsive. Note that this endpoint does not require authentication. Load balancer or other systems can monitor FME Server using this endpoint without supplying token or password credentials.
+	
+Examples:
+# Check if the FME Server is healthy and accepting requests
+fmeserver healthcheck
+
+# Check if the FME Server is healthy and ready to run jobs
+fmeserver healthcheck --ready
+
+
+# `,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// --json overrides --output
-		if jsonOutput {
-			outputType = "json"
-		}
 
 		// set up http
 		client := &http.Client{}
@@ -59,59 +62,14 @@ var healthcheckCmd = &cobra.Command{
 		if err := json.Unmarshal(responseData, &result); err != nil {
 			return err
 		} else {
-			if outputType == "table" {
-				// output all values returned by the JSON in a table
-				v := reflect.ValueOf(result)
-				typeOfS := v.Type()
-				header := table.Row{}
-				row := table.Row{}
-				for i := 0; i < v.NumField(); i++ {
-					header = append(header, typeOfS.Field(i).Name)
-					row = append(row, v.Field(i).Interface())
-				}
-
-				t := table.NewWriter()
-				t.SetStyle(defaultStyle)
-
-				t.AppendHeader(header)
-				t.AppendRow(row)
-
-				if noHeaders {
-					t.ResetHeaders()
-				}
-				fmt.Println(t.Render())
-
+			if !jsonOutput {
+				fmt.Println(result.Status)
 			} else if outputType == "json" {
 				prettyJSON, err := prettyPrintJSON(responseData)
 				if err != nil {
 					return err
 				}
 				fmt.Println(prettyJSON)
-			} else if strings.HasPrefix(outputType, "custom-columns=") {
-				// parse the columns and json queries
-				columnsString := outputType[len("custom-columns="):]
-				if len(columnsString) == 0 {
-					return errors.New("custom-columns format specified but no custom columns given")
-				}
-
-				// we have to marshal the Items array, then create an array of marshalled items
-				// to pass to the creation of the table.
-				marshalledItems := [][]byte{}
-				mJson, err := json.Marshal(result)
-				if err != nil {
-					return err
-				}
-				marshalledItems = append(marshalledItems, mJson)
-
-				columnsInput := strings.Split(columnsString, ",")
-				t, err := createTableFromCustomColumns(marshalledItems, columnsInput)
-				if err != nil {
-					return err
-				}
-				if noHeaders {
-					t.ResetHeaders()
-				}
-				fmt.Println(t.Render())
 			} else {
 				return errors.New("invalid output format specified")
 			}
@@ -124,6 +82,4 @@ var healthcheckCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(healthcheckCmd)
 	healthcheckCmd.Flags().BoolVar(&ready, "ready", false, "The health check will report the status of FME Server if it is ready to process jobs.")
-	healthcheckCmd.Flags().StringVarP(&outputType, "output", "o", "table", "Specify the output type. Should be one of table, json, or custom-columns")
-	healthcheckCmd.Flags().BoolVar(&noHeaders, "no-headers", false, "Don't print column headers")
 }
