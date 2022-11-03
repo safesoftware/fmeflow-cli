028622bec8067f33df67ac17edd5e559b22951e8
Grant Arnold <grant.arnold@safe.com>
2022-11-03 14:43:38 -0700

    Set build number in config file. Dynamically set api version.

--- cmd/healthcheck.go
+++ cmd/healthcheck.go
@@ -9,9 +9,11 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var ready bool
+var apiVersion apiVersionFlag
 
 type HealthcheckV3 struct {
 	Status string `json:"status"`
@@ -43,90 +45,122 @@ fmeserver healthcheck --json`,
 		// set up http
 		client := &http.Client{}
 
-		// This should dynamically be set somehow
-		useV4 := true
+		// get build to decide if we should use v3 or v4
+		// FME Server 2023.0 and later can use v4. Otherwise fall back to v3
+		if apiVersion == "" {
+			fmeserverBuild := viper.GetInt("build")
+			if fmeserverBuild < 23000 {
+				apiVersion = apiVersionFlagV3
+			} else {
+				apiVersion = apiVersionFlagV4
+			}
+		}
+
 		endpoint := ""
-		if useV4 {
+		if apiVersion == "v4" {
 			endpoint = "/fmeapiv4/healthcheck"
 			if ready {
 				endpoint += "/readiness"
 			} else {
 				endpoint += "/liveness"
 			}
-		} else {
-			endpoint = "/fmerest/v3/healthcheck"
-			if ready {
-				endpoint += "?ready=true"
+
+			// call the status endpoint to see if it is finished
+			request, err := buildFmeServerRequest(endpoint, "GET", nil)
+			if err != nil {
+				return err
 			}
-		}
+			response, err := client.Do(&request)
+			if err != nil {
+				return err
+			} else if response.StatusCode != 200 && response.StatusCode != 503 {
+				return errors.New(response.Status)
+			}
 
-		// call the status endpoint to see if it is finished
-		request, err := buildFmeServerRequest(endpoint, "GET", nil)
-		if err != nil {
-			return err
-		}
-		response, err := client.Do(&request)
-		if err != nil {
-			return err
-		} else if response.StatusCode != 200 && response.StatusCode != 503 {
-			return errors.New(response.Status)
-		}
+			responseData, err := io.ReadAll(response.Body)
+			if err != nil {
+				return err
+			}
 
-		responseData, err := io.ReadAll(response.Body)
-		if err != nil {
-			return err
-		}
-		var resultV3 HealthcheckV3
-		var resultV4 HealthcheckV4
-		status := ""
-
-		if useV4 {
+			var resultV4 HealthcheckV4
 			if err := json.Unmarshal(responseData, &resultV4); err != nil {
 				return err
 			}
-			status = resultV4.Status
-		} else {
-			if err := json.Unmarshal(responseData, &resultV3); err != nil {
-				return err
-			}
-			status = resultV3.Status
-		}
-
-		if !jsonOutput {
-			if useV4 {
+			if !jsonOutput {
 				t := createTableWithDefaultColumns(resultV4)
 
 				if noHeaders {
 					t.ResetHeaders()
 				}
 				fmt.Println(t.Render())
+
+			} else if outputType == "json" {
+				prettyJSON, err := prettyPrintJSON(responseData)
+				if err != nil {
+					return err
+				}
+				fmt.Println(prettyJSON)
 			} else {
-				fmt.Println(status)
+				return errors.New("invalid output format specified")
 			}
-		} else if outputType == "json" {
-			prettyJSON, err := prettyPrintJSON(responseData)
-			if err != nil {
-				return err
-			}
-			fmt.Println(prettyJSON)
-		} else {
-			return errors.New("invalid output format specified")
-		}
-		// if the server is unhealthy, make sure we exit with a non-zero error code
-		if useV4 {
 			if response.StatusCode == 503 {
 				os.Exit(1)
 			}
-		} else {
+			return nil
+
+		} else if apiVersion == "v3" {
+			endpoint = "/fmerest/v3/healthcheck"
+			if ready {
+				endpoint += "?ready=true"
+			}
+
+			request, err := buildFmeServerRequest(endpoint, "GET", nil)
+			if err != nil {
+				return err
+			}
+			response, err := client.Do(&request)
+			if err != nil {
+				return err
+			} else if response.StatusCode != 200 {
+				return errors.New(response.Status)
+			}
+
+			responseData, err := io.ReadAll(response.Body)
+			if err != nil {
+				return err
+			}
+			status := ""
+			var resultV3 HealthcheckV3
+			if err := json.Unmarshal(responseData, &resultV3); err != nil {
+				return err
+			}
+			status = resultV3.Status
+			if !jsonOutput {
+				fmt.Println(status)
+			} else if outputType == "json" {
+				prettyJSON, err := prettyPrintJSON(responseData)
+				if err != nil {
+					return err
+				}
+				fmt.Println(prettyJSON)
+			} else {
+				return errors.New("invalid output format specified")
+			}
+			// if the server is unhealthy, make sure we exit with a non-zero error code
 			if status != "ok" {
 				os.Exit(1)
 			}
+			return nil
+		} else {
+			return fmt.Errorf("invalid apiVersion")
 		}
-		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(healthcheckCmd)
 	healthcheckCmd.Flags().BoolVar(&ready, "ready", false, "The health check will report the status of FME Server if it is ready to process jobs.")
+	healthcheckCmd.Flags().Var(&apiVersion, "api-version", "The api version to use when contacting FME Server. Must be one of v3 or v4")
+	healthcheckCmd.Flags().MarkHidden("api-version")
+	healthcheckCmd.RegisterFlagCompletionFunc("api-version", apiVersionFlagCompletion)
 }
