f5c714d1b11e5f8417decaaf9608d03afb5d0f94
Amber Xu <amber.xu@safe.com>
2025-09-03 14:28:11 -0700

    engines command with REST v4

--- cmd/engines.go
+++ cmd/engines.go
@@ -11,9 +11,27 @@ import (
 	"github.com/jedib0t/go-pretty/v6/table"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-type Engine struct {
+type EngineV4 struct {
+	Name                   string   `json:"name"`
+	Hostname               string   `json:"hostname"`
+	EngineManagerHostname  string   `json:"engineManagerHostname"`
+	Platform               string   `json:"platform"`
+	CurrentJobID           int      `json:"currentJobID"`
+	BuildNumber            int      `json:"buildNumber"`
+	Type                   string   `json:"type"`
+	State                  string   `json:"state"`
+	AssignedQueues         []string `json:"assignedQueues"`
+	RegistrationProperties []string `json:"registrationProperties"`
+	HostProperties         struct {
+		PhysicalMemory int `json:"physicalMemory"`
+		ProcessorCount int `json:"processorCount"`
+	} `json:"hostProperties"`
+}
+
+type EngineV3 struct {
 	HostName                    string        `json:"hostName"`
 	AssignedQueues              []string      `json:"assignedQueues"`
 	ResultFailureCount          int           `json:"resultFailureCount"`
@@ -30,17 +48,26 @@ type Engine struct {
 	TransactionPort             int           `json:"transactionPort"`
 	CurrentJobID                int           `json:"currentJobID"`
 }
-type Engines struct {
-	Offset     int      `json:"offset"`
-	Limit      int      `json:"limit"`
-	TotalCount int      `json:"totalCount"`
-	Items      []Engine `json:"items"`
+
+type EnginesV4 struct {
+	Offset     int        `json:"offset"`
+	Limit      int        `json:"limit"`
+	TotalCount int        `json:"totalCount"`
+	Items      []EngineV4 `json:"items"`
+}
+
+type EnginesV3 struct {
+	Offset     int        `json:"offset"`
+	Limit      int        `json:"limit"`
+	TotalCount int        `json:"totalCount"`
+	Items      []EngineV3 `json:"items"`
 }
 
 type engineFlags struct {
 	count      bool
 	outputType string
 	noHeaders  bool
+	apiVersion apiVersionFlag
 }
 
 // enginesCmd represents the engines command
@@ -68,6 +95,7 @@ func newEnginesCmd() *cobra.Command {
 	cmd.Flags().StringVarP(&f.outputType, "output", "o", "table", "Specify the output type. Should be one of table, json, or custom-columns")
 	cmd.Flags().BoolVar(&f.noHeaders, "no-headers", false, "Don't print column headers")
 	cmd.Flags().BoolVar(&f.count, "count", false, "Prints the total count of engines.")
+	cmd.Flags().Var(&f.apiVersion, "api-version", "The api version to use when contacting FME Server. Must be one of v3 or v4")
 	cmd.MarkFlagsMutuallyExclusive("output", "count")
 	cmd.MarkFlagsMutuallyExclusive("no-headers", "count")
 	//enginesCmd.MarkFlagsMutuallyExclusive("json", "count")
@@ -81,90 +109,206 @@ func enginesRun(f *engineFlags) func(cmd *cobra.Command, args []string) error {
 		if jsonOutput {
 			f.outputType = "json"
 		}
+
+		if f.apiVersion == "" {
+			if viper.GetInt("build") < projectsV4BuildThreshold {
+				f.apiVersion = apiVersionFlagV3
+			} else {
+				f.apiVersion = apiVersionFlagV4
+			}
+		}
+
 		// set up http
 		client := &http.Client{}
 
-		// call the status endpoint to see if it is finished
-		request, err := buildFmeFlowRequest("/fmerest/v3/transformations/engines", "GET", nil)
-		if err != nil {
-			return err
-		}
-		response, err := client.Do(&request)
-		if err != nil {
-			return err
-		} else if response.StatusCode != 200 {
-			return errors.New(response.Status)
-		}
+		if f.apiVersion == "v4" {
+			request, err := buildFmeFlowRequest("/fmeapiv4/engines", "GET", nil)
+			if err != nil {
+				return err
+			}
+			response, err := client.Do(&request)
+			if err != nil {
+				return err
+			} else if response.StatusCode != http.StatusOK {
+				responseData, err := io.ReadAll(response.Body)
+				if err == nil {
+					var responseMessage Message
+					if err := json.Unmarshal(responseData, &responseMessage); err == nil {
 
-		responseData, err := io.ReadAll(response.Body)
-		if err != nil {
-			return err
-		}
-
-		var result Engines
-		if err := json.Unmarshal(responseData, &result); err != nil {
-			return err
-		} else {
-			if f.count {
-				// simply return the count of engines
-				fmt.Fprintln(cmd.OutOrStdout(), result.TotalCount)
-			} else if f.outputType == "table" { // output a table with some default fields selected
-				t := table.NewWriter()
-				t.SetStyle(defaultStyle)
-
-				t.AppendHeader(table.Row{"Name", "Host", "Build", "Platform", "Type", "Current Job ID", "Registration Properties", "Queues"})
-
-				for _, element := range result.Items {
-					t.AppendRow(table.Row{element.InstanceName, element.HostName, element.BuildNumber, element.Platform, element.Type, element.CurrentJobID, element.RegistrationProperties, element.AssignedQueues})
+						// if json output is requested, output the JSON to stdout before erroring
+						if jsonOutput {
+							prettyJSON, err := prettyPrintJSON(responseData)
+							if err == nil {
+								fmt.Fprintln(cmd.OutOrStdout(), prettyJSON)
+							} else {
+								return errors.New(response.Status)
+							}
+						}
+						return errors.New(responseMessage.Message)
+					} else {
+						return errors.New(response.Status)
+					}
+				} else {
+					return errors.New(response.Status)
 				}
-				if f.noHeaders {
-					t.ResetHeaders()
-				}
-				fmt.Fprintln(cmd.OutOrStdout(), t.Render())
-				// output the raw json but formatted
-			} else if f.outputType == "json" {
-				prettyJSON, err := prettyPrintJSON(responseData)
-				if err != nil {
-					return err
-				}
-				fmt.Fprintln(cmd.OutOrStdout(), prettyJSON)
+			}
 
-			} else if strings.HasPrefix(f.outputType, "custom-columns") {
-				// parse the columns and json queries
-				columnsString := ""
-				if strings.HasPrefix(f.outputType, "custom-columns=") {
-					columnsString = f.outputType[len("custom-columns="):]
-				}
-				if len(columnsString) == 0 {
-					return errors.New("custom-columns format specified but no custom columns given")
-				}
+			// unmarshal into struct
+			var result EnginesV4
+			responseData, err := io.ReadAll(response.Body)
+			if err != nil {
+				return err
+			}
+			if err := json.Unmarshal(responseData, &result); err != nil {
+				return err
+			} else {
+				if f.count {
+					// simply return the count of engines
+					fmt.Fprintln(cmd.OutOrStdout(), result.TotalCount)
+				} else if f.outputType == "table" { // output a table with some default fields selected
+					t := table.NewWriter()
+					t.SetStyle(defaultStyle)
 
-				// we have to marshal the Items array, then create an array of marshalled items
-				// to pass to the creation of the table.
-				marshalledItems := [][]byte{}
-				for _, element := range result.Items {
-					mJson, err := json.Marshal(element)
+					t.AppendHeader(table.Row{"Name", "Host", "Build", "Platform", "Type", "Current Job ID", "Registration Properties", "Queues"})
+
+					for _, element := range result.Items {
+						t.AppendRow(table.Row{element.Name, element.Hostname, element.BuildNumber, element.Platform, element.Type, element.CurrentJobID, element.RegistrationProperties, element.AssignedQueues})
+					}
+					if f.noHeaders {
+						t.ResetHeaders()
+					}
+					fmt.Fprintln(cmd.OutOrStdout(), t.Render())
+					// output the raw json but formatted
+				} else if f.outputType == "json" {
+					prettyJSON, err := prettyPrintJSON(responseData)
 					if err != nil {
 						return err
 					}
+					fmt.Fprintln(cmd.OutOrStdout(), prettyJSON)
 
-					marshalledItems = append(marshalledItems, mJson)
+				} else if strings.HasPrefix(f.outputType, "custom-columns") {
+					// parse the columns and json queries
+					columnsString := ""
+					if strings.HasPrefix(f.outputType, "custom-columns=") {
+						columnsString = f.outputType[len("custom-columns="):]
+					}
+					if len(columnsString) == 0 {
+						return errors.New("custom-columns format specified but no custom columns given")
+					}
+
+					// we have to marshal the Items array, then create an array of marshalled items
+					// to pass to the creation of the table.
+					marshalledItems := [][]byte{}
+					for _, element := range result.Items {
+						mJson, err := json.Marshal(element)
+						if err != nil {
+							return err
+						}
+
+						marshalledItems = append(marshalledItems, mJson)
+					}
+
+					columnsInput := strings.Split(columnsString, ",")
+					t, err := createTableFromCustomColumns(marshalledItems, columnsInput)
+					if err != nil {
+						return err
+					}
+					if f.noHeaders {
+						t.ResetHeaders()
+					}
+					fmt.Fprintln(cmd.OutOrStdout(), t.Render())
+
+				} else {
+					return errors.New("invalid output format specified")
 				}
 
-				columnsInput := strings.Split(columnsString, ",")
-				t, err := createTableFromCustomColumns(marshalledItems, columnsInput)
-				if err != nil {
-					return err
-				}
-				if f.noHeaders {
-					t.ResetHeaders()
-				}
-				fmt.Fprintln(cmd.OutOrStdout(), t.Render())
-
-			} else {
-				return errors.New("invalid output format specified")
 			}
 
+			return nil
+		} else if f.apiVersion == "v3" {
+			// call the status endpoint to see if it is finished
+			request, err := buildFmeFlowRequest("/fmerest/v3/transformations/engines", "GET", nil)
+			if err != nil {
+				return err
+			}
+			response, err := client.Do(&request)
+			if err != nil {
+				return err
+			} else if response.StatusCode != 200 {
+				return errors.New(response.Status)
+			}
+
+			responseData, err := io.ReadAll(response.Body)
+			if err != nil {
+				return err
+			}
+
+			var result EnginesV3
+			if err := json.Unmarshal(responseData, &result); err != nil {
+				return err
+			} else {
+				if f.count {
+					// simply return the count of engines
+					fmt.Fprintln(cmd.OutOrStdout(), result.TotalCount)
+				} else if f.outputType == "table" { // output a table with some default fields selected
+					t := table.NewWriter()
+					t.SetStyle(defaultStyle)
+
+					t.AppendHeader(table.Row{"Name", "Host", "Build", "Platform", "Type", "Current Job ID", "Registration Properties", "Queues"})
+
+					for _, element := range result.Items {
+						t.AppendRow(table.Row{element.InstanceName, element.HostName, element.BuildNumber, element.Platform, element.Type, element.CurrentJobID, element.RegistrationProperties, element.AssignedQueues})
+					}
+					if f.noHeaders {
+						t.ResetHeaders()
+					}
+					fmt.Fprintln(cmd.OutOrStdout(), t.Render())
+					// output the raw json but formatted
+				} else if f.outputType == "json" {
+					prettyJSON, err := prettyPrintJSON(responseData)
+					if err != nil {
+						return err
+					}
+					fmt.Fprintln(cmd.OutOrStdout(), prettyJSON)
+
+				} else if strings.HasPrefix(f.outputType, "custom-columns") {
+					// parse the columns and json queries
+					columnsString := ""
+					if strings.HasPrefix(f.outputType, "custom-columns=") {
+						columnsString = f.outputType[len("custom-columns="):]
+					}
+					if len(columnsString) == 0 {
+						return errors.New("custom-columns format specified but no custom columns given")
+					}
+
+					// we have to marshal the Items array, then create an array of marshalled items
+					// to pass to the creation of the table.
+					marshalledItems := [][]byte{}
+					for _, element := range result.Items {
+						mJson, err := json.Marshal(element)
+						if err != nil {
+							return err
+						}
+
+						marshalledItems = append(marshalledItems, mJson)
+					}
+
+					columnsInput := strings.Split(columnsString, ",")
+					t, err := createTableFromCustomColumns(marshalledItems, columnsInput)
+					if err != nil {
+						return err
+					}
+					if f.noHeaders {
+						t.ResetHeaders()
+					}
+					fmt.Fprintln(cmd.OutOrStdout(), t.Render())
+
+				} else {
+					return errors.New("invalid output format specified")
+				}
+
+			}
+			return nil
 		}
 		return nil
 	}
