6adb61daafc3b6bca2292aa405db2ee1d2783e1c
Grant Arnold <grant.arnold@safe.com>
2025-10-24 22:32:31 +0000

    Update systemcode command to not run on older builds.

--- cmd/systemcode.go
+++ cmd/systemcode.go
@@ -8,11 +8,14 @@ import (
 	"net/http"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 type SystemCode struct {
 	SystemCode string `json:"systemCode"`
 }
+
+var systemCodeDeprecatedBuildThreshold = 26000
 
 func newSystemCodeCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -27,6 +30,12 @@ func newSystemCodeCmd() *cobra.Command {
 
 func systemCodeRun() func(cmd *cobra.Command, args []string) error {
 	return func(cmd *cobra.Command, args []string) error {
+		// Check if systemcode is available in this version of FME Flow
+		fmeflowBuild := viper.GetInt("build")
+		if fmeflowBuild >= systemCodeDeprecatedBuildThreshold {
+			return errors.New("systemcode is not available in this version of FME Flow. The systemcode command was removed in FME Flow 2026.1+")
+		}
+
 		// set up http
 		client := &http.Client{}
 
