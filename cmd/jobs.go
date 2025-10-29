f1cf58635209337474d7eeabb97db513dceb5e5f
Grant Arnold <grant.arnold@safe.com>
2025-10-29 21:38:56 +0000

    Update source-id and source-type descriptions and examples.

--- cmd/jobs.go
+++ cmd/jobs.go
@@ -132,7 +132,12 @@ func newJobsCmd() *cobra.Command {
 		Use:   "jobs",
 		Short: "Lists jobs on FME Server",
 		Long:  "Lists running, queued, and/or queued jobs on FME Server. Pass in a job id to get information on a specific job.",
-
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if f.jobsSourceID != "" && f.jobsSourceType == "" {
+				return errors.New("--source-id requires --source-type to be specified")
+			}
+			return nil
+		},
 		Example: `
 
   # List all jobs (currently limited to the most recent 1000)
@@ -156,6 +161,12 @@ func newJobsCmd() *cobra.Command {
 	
   # List all jobs in JSON format
   fmeflow jobs --json
+
+  # List all jobs run from an automation with id 63f2489a-f3fc-4fa0-8df8-198de602b922
+  fmeflow jobs --source-type automations --source-id 63f2489a-f3fc-4fa0-8df8-198de602b922
+
+  # List all jobs run from a schedule with id 32f819b6-b3dc-4cff-a320-8c56a7c81163 in JSON format
+  fmeflow jobs --source-type schedules --source-id 32f819b6-b3dc-4cff-a320-8c56a7c81163 --json
 	
   # List the workspace, CPU time and peak memory usage for a given repository
   fmeflow jobs --repository Samples --output="custom-columns=WORKSPACE:.workspace,CPU Time:.cpuTime"
@@ -179,8 +190,8 @@ func newJobsCmd() *cobra.Command {
 	cmd.Flags().StringVar(&f.jobsRepository, "repository", "", "If specified, only jobs from the specified repository will be returned")
 	cmd.Flags().StringVar(&f.jobsWorkspace, "workspace", "", "If specified along with repository, only jobs from the specified repository and workspace will be returned")
 	cmd.Flags().StringVar(&f.jobsUserName, "user-name", "", "If specified, only jobs run by the specified user will be returned")
-	cmd.Flags().StringVar(&f.jobsSourceID, "source-id", "", "If specified along with source type, only jobs from the specified type with the specified id will be returned. For Automations, the source id is the automation id. For WorkspaceSubscriber, the source id is the id of the subscription. For Scheduler, the source id is the category and name of the schedule separated by '/'. For example, 'Category/Name'")
-	cmd.Flags().StringVar(&f.jobsSourceType, "source-type", "", "If specified, only jobs run by this source type will be returned")
+	cmd.Flags().StringVar(&f.jobsSourceID, "source-id", "", "If specified along with source type, only jobs that were run from the source with the specified id will be returned.")
+	cmd.Flags().StringVar(&f.jobsSourceType, "source-type", "", "If specified, only jobs run by this source type will be returned. One of: automations, workspaceSubscriptions, schedules, workspaceApps, automationApps, streams, fmeflow, dataVirtualization.")
 	cmd.Flags().StringVarP(&f.outputType, "output", "o", "table", "Specify the output type. Should be one of table, json, or custom-columns")
 	cmd.Flags().IntVar(&f.jobId, "id", -1, "Specify the job id to display")
 	cmd.Flags().BoolVar(&f.noHeaders, "no-headers", false, "Don't print column headers")
